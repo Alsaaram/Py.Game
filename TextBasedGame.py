53c1b627f327866c824c73b78cb469756a52763f
Ibrahim <8332931+ibrahimbutt@users.noreply.github.com>
2023-04-23 13:52:45 +0100

    Refactor `get_new_state`

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -53,12 +53,39 @@ state = STARTING_POSITION
 
 # For movement around the map
 def get_new_state(state, direction):
-    new_state = state
-    for i in rooms:
-        if i == state:
-            if direction in rooms[i]:
-                new_state = rooms[i][direction]
-    return new_state
+    # What if the room or direction does not exist?
+
+    # If you just return the existing position, how can the programme determine something went wrong?
+    # The name of this function suggests a new state will be returned, which is misleading as it's not a guarantee.
+
+    # Two approaches here:
+    # 1. Before calling this function, validate that the position is and direction are valid inputs. If not, ask user
+    #    for new ones.
+    # 2. Validate inside this function and return a tuple. E.g. `return true, state`. Returning `true` will signal
+    #    that it was valid request, and return the new state. Returning `false` will signal that it was invalid, and
+    #    return the existing state. You will need to check for true and false after this is function is called. I don't
+    #    know Python best practices, so not sure if this is a good approach or not. However, it is a common pattern
+    #    in Go.
+
+    # Personally, I would have a validator, which follows the Single Responsibility Principle.
+    # See: https://en.wikipedia.org/wiki/Single-responsibility_principle
+
+    # Below I've combined the two if statements with an `and` clause. Generally, nesting `if` statements is a code
+    # smell, but it can't always be avoided. As a rule of thumb, aim for no nesting. Too many if statements is a sign of
+    # too much being handled by a piece of code, and that functionality should be abstracted.
+
+    # There was no need to loop over the rooms. It's not an array. It's a dictionary, which gives you instant lookups.
+    # You just need the key to get the room and its directions. The key is being passed in to this function.
+    # mind that it might not be valid.
+
+    # You used `i` for looping. For arrays that is ok as it represents an index, but when looping over objects, you
+    # should use `k` or `key`.
+
+    room = rooms.get(state)
+    if room and direction in room:
+        return room[direction]
+
+    return state
 
 
 def get_items(state):
